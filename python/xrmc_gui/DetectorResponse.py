ff4ef71c0a92cafff13b1c5de8699cd7bcedc8ff
linssab <sergio.lins@roma3.infn.it>
2021-03-16 14:25:05 +0100

    Added detector correction (always considers tail) - escape peaks not yet corrected

---
+++ python/xrmc_gui/DetectorResponse.py
@@ -0,0 +1,163 @@
+import numpy as np
+import sys, os
+from math import erfc
+try: import xraylib as xlib
+except: 
+    print("Failed to import xraylib. Aborting...")
+    sys.exit(1)
+
+class rivelatore:
+    def __init__(__self__):
+        __self__.Detect_Z = int
+        __self__.Detect_thickness = float
+        __self__.Detect_window_Z = int
+        __self__.Detect_window_thickness = float
+        __self__.FWHM_start = float
+        __self__.FWHM_stop = float
+        __self__.AirThick = float
+        __self__.MCA_chns = int
+        __self__.noise = float
+        __self__.Fano = float
+        __self__.EscapePeakPercentage = float
+        __self__.energy_max = float
+        __self__.scatt_orders =  int
+        __self__.tail_model = bool
+        __self__.EscapePeak = bool
+
+    def setup(__self__, w):
+        __self__.Detect_Z = int(w.Z.get())
+        __self__.Detect_thickness = float(w.thickness.get())
+        __self__.Detect_window_Z = int(w.win_z.get())
+        __self__.Detect_window_thickness = float(w.win_thickness.get())
+        __self__.FWHM_start = float(w.fstart.get())
+        __self__.FWHM_stop = float(w.fstop.get())
+        __self__.AirThick = float(w.air_thickness.get())
+        __self__.noise = float(w.noise.get())
+        __self__.Fano = float(w.fano.get())
+        __self__.EscapePeakPercentage = float(w.escape_perc.get())
+        __self__.tail_model = bool(w.tail.get())
+        __self__.EscapePeak = bool(w.escape.get())
+
+def detector_efficiency_convolution(
+        data,
+        spectrum_size,
+        scatt_orders,
+        rivelatore):
+    
+    Z = rivelatore.Detect_Z
+    thickness = rivelatore.Detect_thickness
+    Z_window = rivelatore.Detect_window_Z
+    window_thick = rivelatore.Detect_window_thickness
+    energy_max = rivelatore.energy_max
+
+    #FILE *file_in,*file_out;
+    i,j,k = 0,0,0
+    sigma_var = 0
+    density, density1 = 0, 0 
+    density_air = 0.0012
+    attenuation = 0
+    attenuation_window = 0 
+    attenuation_air = 0
+    energy = 0
+    air_thickness = rivelatore.AirThick
+
+    #xrl_error **error;
+
+    #if (xraylib_init_flag==false){
+    #	  XRayInit();
+    #xraylib_init_flag=true;
+    #}
+
+    spectrum_temp = np.zeros(spectrum_size, dtype=np.double)
+    spectrum_conv = np.zeros([scatt_orders,spectrum_size], dtype=np.double)
+
+    #file_in=fopen(spectrum_in,"rb");
+    #file_out=fopen(spectrum_out,"wb");
+    #fread(spectrum_temp,sizeof(double),spectrum_size,file_in);
+    #fwrite(spectrum_temp,sizeof(double),spectrum_size,file_out);
+
+    k=0
+    print("Efficiency correction")
+    for k in range(scatt_orders):
+	#fread(spectrum_temp,sizeof(double),spectrum_size,file_in);
+        spectrum_temp = data[k,:] 
+        density = xlib.ElementDensity(Z)
+        density1 = xlib.ElementDensity(Z_window)
+        i=0
+        for i in range(spectrum_size): 
+            energy = i*rivelatore.energy_max/rivelatore.MCA_chns
+            print(f"{energy} - Progress {k}: {i}/{spectrum_size}", end="\r")
+            if energy > 0:
+                attenuation = xlib.CS_Total(Z, energy)
+                attenuation_window = xlib.CS_Total(Z_window, energy)
+                attenuation_air = 0.7804 * xlib.CS_Total(7, energy) + 0.20946 * xlib.CS_Total(8, energy) + 0.00934 * xlib.CS_Total(18, energy)
+                spectrum_conv[k][i] = spectrum_temp[i] * (1.0-np.exp(-attenuation*thickness*density)) * np.exp(-attenuation_window * window_thick * density1-attenuation_air * air_thickness*density_air)
+            else:
+                spectrum_conv[k][i] = 0.0
+	  #fwrite(spectrum_conv,sizeof(double),spectrum_size,file_out);
+    return spectrum_conv
+
+def SDD_convolution_with_tail(
+        data,
+        num_chns, 
+        scatt_orders,
+        rivelatore):
+
+    #FILE *file_in,*file_out;
+    i,j,k,m = 0,0,0,0
+    sigma_var, my_sum = 0, 0
+    factor = 0
+    spectrum_temp = np.zeros(num_chns, dtype=np.double)
+    spectrum_conv = np.zeros([scatt_orders,num_chns], dtype=np.double)
+    R = np.zeros(num_chns, dtype=np.double)
+    step = rivelatore.energy_max/rivelatore.MCA_chns
+
+    A0,A3,A4,A5,B0,X,FWHM = 0,0,0,0,0,0,0
+    alpha,a,b,E0,G,G1,F,E_curr = 0,0,0,0,0,0,0,0
+    chan_ref = 0
+
+    #file_in=fopen(spectrum_in,"rb");
+
+    #file_out=fopen(spectrum_out,"wb");
+    #fread(spectrum_temp,sizeof(double),spectrum_size,file_in);
+    #fwrite(spectrum_temp,sizeof(double),spectrum_size,file_out);
+
+    print("SDD convolution")
+    for m in range(scatt_orders):
+
+        #fread(spectrum_temp,sizeof(double),spectrum_size,file_in);
+
+        spectrum_temp = data[m,:,0,0] 
+
+        a=rivelatore.noise*rivelatore.noise;
+        b=2.3548*2.3548*3.85*rivelatore.Fano/1000.0;
+
+        for i in range(1,num_chns):
+            print(f"Progress {m}: {i}/{num_chns}", end="\r")
+            my_sum=0.0;
+            E0=i*step;
+            FWHM=np.sqrt(a+b*E0);
+            B0=np.sqrt(2.0)/(2.0*np.sqrt(2.0*np.log(2.0)))*FWHM;
+            #A0=1.0/(FWHM*np.sqrt(np.pi));
+            #B0 = 0.6005612*FWHM
+            A0=1.0/(FWHM*1.77245385);
+            A3=2.73E-3*np.exp(-0.21*E0)+1.0E-4;
+            A4=0.000188*np.exp(-0.00296*(pow(E0,0.763)))+1.355E-5*np.exp(0.968*(pow(E0,0.498)));
+            alpha=1.179*np.exp(8.6E-4*(pow(E0,1.877)))- 7.793*np.exp(-3.81/(pow(E0,0.0716)));
+            for j in range(1,i+100):
+                if j < rivelatore.MCA_chns:
+                    E_curr=step*j;
+                    X=(E_curr-E0)/B0;
+                    G=np.exp(-X*X);
+                    #G1=exp(-(E_curr-(E0-1.74))*(E_curr-(E0-1.74))/(2*FWHM*FWHM));
+                    F=erfc(X);
+                    R[j]=A0*G+0.63*A3+15.0*A4*np.exp(alpha*(E_curr-E0))*F;
+                    my_sum+=R[j];
+                else: break
+            for k in range(1,i+100):
+                if k < rivelatore.MCA_chns:
+                    spectrum_conv[m][k]+=R[k]*spectrum_temp[i]/my_sum;
+                else: break
+          #fwrite(spectrum_conv,sizeof(double),rivelatore.MCA_chns,file_out);
+    return spectrum_conv
+
--- python/xrmc_gui/__main__.py
+++ python/xrmc_gui/__main__.py
@@ -1,19 +1,22 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
+from tkinter import filedialog
 import subprocess, sys, os
 import xrmc
+from DetectorResponse import *
 import Configure
 import Elements
 import matplotlib.pyplot as plt
 from subprocess import Popen, PIPE
+
+global DETECTOR
 
 class CustomCompound:
     def __init__(__self__, root=None):
         __self__.master = Toplevel(master=root.master)
         __self__.master.resizable(False,False)
         __self__.master.title("Custom coumpound maker")
-
         __self__.build_widgets
 
     def build_widgets(__self__):
@@ -21,17 +24,124 @@ class CustomCompound:
         __self__.frame.grid(row=0, column=0)
         label = Label(__self__.frame, text="UNDER CONSTRUCTION").grid(row=0,column=0)
 
+class SDD_window:
+    def __init__(__self__,parent):
+        __self__.master = Toplevel(master=parent.master)
+        __self__.parent = parent
+        __self__.Frame = ttk.LabelFrame(__self__.master, text="Detector properties")
+        __self__.Labels = Frame(__self__.Frame, padx=10, pady=10)
+        __self__.Buttons = Frame(__self__.Frame, padx=15, pady=10)
+        __self__.Frame.grid(padx=15, pady=15)
+        __self__.Labels.grid(row=0,column=0)
+        __self__.Buttons.grid(row=1,column=0,columnspan=2)
+        __self__.init_variables()
+        __self__.write_from_init()
+        __self__.build_widgets()
+    
+    def init_variables(__self__):
+        __self__.Z = IntVar()
+        __self__.thickness = DoubleVar()
+        __self__.win_z = IntVar()
+        __self__.win_thickness = DoubleVar()
+        __self__.fstart = DoubleVar()
+        __self__.fstop = DoubleVar()
+        __self__.air_thickness = DoubleVar()
+        __self__.noise = DoubleVar()
+        __self__.fano = DoubleVar()
+        __self__.escape_perc = DoubleVar()
+        __self__.tail = BooleanVar()
+        __self__.escape = BooleanVar()
+
+    def kill(__self__):
+        __self__.master.grab_release()
+        __self__.parent.master.focus_set()
+        __self__.master.destroy()
+
+    def save(__self__):
+        print("saving...")
+        global DETECTOR
+        DETECTOR = rivelatore()
+        DETECTOR.setup(__self__)
+        __self__.kill()
+
+    def write_from_init(__self__):
+        p = os.path.join(os.path.dirname(__file__),"detector.ini")
+        if os.path.exists(p):
+            pass
+        else:
+            return
+        f = open(p,"r")
+        lines = f.readlines()
+        for line in lines:
+            line = line.replace("\n","")
+            if "DETECTOR_Z" in line: __self__.Z.set(int(line.split("=")[-1]))
+            elif "MATERIAL_THICK" in line: __self__.thickness.set(float(line.split("=")[-1]))
+            elif "WINDOW_Z" in line: __self__.win_z.set(int(line.split("=")[-1]))
+            elif "WINDOW_THICK" in line: __self__.win_thickness.set(float(line.split("=")[-1]))
+            elif "FWHM_START" in line: __self__.fstart.set(float(line.split("=")[-1]))
+            elif "FWHM_STOP" in line: __self__.fstop.set(float(line.split("=")[-1]))
+            elif "AIR_THICK" in line: __self__.air_thickness.set(float(line.split("=")[-1]))
+            elif "NOISE" in line: __self__.noise.set(float(line.split("=")[-1]))
+            elif "FANO" in line: __self__.fano.set(float(line.split("=")[-1]))
+            elif "ESCAPE_PERC" in line: __self__.escape_perc.set(float(line.split("=")[-1]))
+            elif "TAIL" in line: __self__.tail.set(bool(int(line.split("=")[-1])))
+            elif "ESCAPE" in line: __self__.escape.set(bool(int(line.split("=")[-1])))
+        return
+
+    def build_widgets(__self__):
+        Label(__self__.Labels, text="Detector Z").grid(row=0, column=0, sticky=W)
+        Label(__self__.Labels, text="Material thickness").grid(row=1, column=0, sticky=W)
+        Label(__self__.Labels, text="Window Z").grid(row=2, column=0, sticky=W)
+        Label(__self__.Labels, text="Window thickness").grid(row=3, column=0, sticky=W)
+        Label(__self__.Labels, text="FWHM start").grid(row=4, column=0, sticky=W)
+        Label(__self__.Labels, text="FWHM stop").grid(row=5, column=0, sticky=W)
+        Label(__self__.Labels, text="Air thickness").grid(row=6, column=0, sticky=W)
+        Label(__self__.Labels, text="Detector noise").grid(row=7, column=0, sticky=W)
+        Label(__self__.Labels, text="Fano factor").grid(row=8, column=0, sticky=W)
+        Label(__self__.Labels, text="Escape peak (%) 0-1").grid(row=9,column=0, sticky=W)
+        ttk.Entry(__self__.Labels, textvariable=__self__.Z).grid(row=0, column=1)
+        ttk.Entry(__self__.Labels, textvariable=__self__.thickness).grid(row=1, column=1)
+        ttk.Entry(__self__.Labels, textvariable=__self__.win_z).grid(row=2, column=1)
+        ttk.Entry(__self__.Labels, textvariable=__self__.win_thickness).grid(row=3, column=1)
+        ttk.Entry(__self__.Labels, textvariable=__self__.fstart).grid(row=4, column=1)
+        ttk.Entry(__self__.Labels, textvariable=__self__.fstop).grid(row=5, column=1)
+        ttk.Entry(__self__.Labels, textvariable=__self__.air_thickness).grid(row=6, column=1)
+        ttk.Entry(__self__.Labels, textvariable=__self__.noise).grid(row=7, column=1)
+        ttk.Entry(__self__.Labels, textvariable=__self__.fano).grid(row=8, column=1)
+        ttk.Entry(__self__.Labels, textvariable=__self__.escape_perc).grid(row=9, column=1)
+        ttk.Checkbutton(__self__.Buttons, text="Use tail model", variable=__self__.tail).grid(
+                row=0, column=0, padx=(0,10))
+        ttk.Checkbutton(__self__.Buttons, text="Escape peaks corr", 
+                variable=__self__.escape).grid(row=0, column=1, padx=(10,0))
+        ttk.Button(__self__.Buttons, text="Cancel", command=__self__.kill).grid(
+                row=1, column=0, pady=(15,0))
+        ttk.Button(__self__.Buttons, text="Save", command=__self__.save).grid(
+                row=1, column=1, pady=(15,0))
+        __self__.master.update_idletasks()
+        __self__.master.focus_set()
+        __self__.master.grab_set()
+
 
 class Root:
     def __init__(__self__):
         __self__.master = Tk()
         __self__.master.resizable(False, False)
+        __self__.menu = Menu(__self__.master, tearoff=0)
+        __self__.dropdown = Menu(__self__.menu, tearoff=0)
+        __self__.dropdown.add_command(label="Configure detector", 
+                command=__self__.configure_SDD)
+        __self__.menu.add_cascade(label="Options", menu=__self__.dropdown)
+        __self__.master.config(menu=__self__.menu)
         __self__.layer_count = int(0)
         __self__.info = StringVar()
         __self__.layers = []
         __self__.thicknesses = []
         __self__.comboboxes = []
         __self__.build_widgets()
+
+    def configure_SDD(__self__):
+        SDD_window(__self__)        
+        return
 
     def build_widgets(__self__):
         __self__.frame = LabelFrame(__self__.master, text="Main Panel",padx=6,pady=6)
@@ -42,11 +152,6 @@ class Root:
                 anchor=W,
                 relief=SUNKEN)
 
-        __self__.frame.grid(row=0, column=0, padx=3, pady=3,sticky=N+S)
-        __self__.frame2.grid(row=0, column=1, padx=3, pady=3,sticky=N+S)
-        __self__.buttons.grid(row=1, padx=3, pady=3)
-        __self__.statusbox.grid(row=2, columnspan=2, sticky=W+E)
-
         Label(__self__.frame, text="Layer Material").grid(row=0,column=0,columnspan=2)
         Label(__self__.frame, text="Layer\nThickness (um)").grid(row=0,column=2)
         __self__.add = ttk.Button(__self__.frame, text="Add Layer",
@@ -55,19 +160,26 @@ class Root:
         w = 7
         __self__.species = Listbox(__self__.frame2, width=w)
         __self__.quantity = Listbox(__self__.frame2, width=w+3)
-        __self__.species.grid(row=0, column=0, sticky=N+S)
-        __self__.quantity.grid(row=0, column=1, sticky=N+S)
 
         __self__.insert_layer()
 
+        __self__.EraseVar = BooleanVar()
+        __self__.EraseVar.set(1)
+        __self__.erase = ttk.Checkbutton(__self__.buttons, text="Erase generated files", variable=__self__.EraseVar)
         __self__.accept = ttk.Button(__self__.buttons, text="Go!",
                 command=__self__.launch)
         __self__.cancel = ttk.Button(__self__.buttons, text="Cancel",
                 command=__self__.master.destroy)
 
+        __self__.frame.grid(row=0, column=0, padx=3, pady=3,sticky=N+S)
+        __self__.frame2.grid(row=0, column=1, padx=3, pady=3,sticky=N+S)
+        __self__.buttons.grid(row=1, padx=3, pady=3)
+        __self__.statusbox.grid(row=2, columnspan=2, sticky=W+E)
+        __self__.species.grid(row=0, column=0, sticky=N+S)
+        __self__.quantity.grid(row=0, column=1, sticky=N+S)
         __self__.accept.grid(row=0, column=0)
         __self__.cancel.grid(row=0, column=1)
-
+        __self__.erase.grid(row=0,column=2)
         return
 
     def insert_layer(__self__, e=""):
@@ -109,6 +221,8 @@ class Root:
         return
 
     def launch(__self__,e=""):
+        global DETECTOR
+
         compounds, materials, thicknesses = [], [], []
         for i in range(len(__self__.layers)):
             if __self__.layers[i].get() \
@@ -126,6 +240,7 @@ class Root:
         input_file, output_file = pars.write_inputs(compounds)
 
         exe_str = r"xrmc {}".format(input_file)
+        print("SIMULATION START!")
         p1 = subprocess.Popen(exe_str,  stderr=subprocess.PIPE)#, stdout=subprocess.PIPE)
         #while True:
         #    p1.stdout.flush()
@@ -134,16 +249,54 @@ class Root:
 
         p1.wait()
         print("SIMULATION DONE")
-        for f in pars.temporary_files:
-            #os.remove(f)
-            print(f"Got rid of {f}")
+        if __self__.EraseVar.get():
+            for f in pars.temporary_files:
+                try: 
+                    os.remove(f)
+                    print(f"Got rid of {f}")
+                except:
+                    print(f"Could not remove {f}!")
 
         print("Output file: ", output_file)
         data = xrmc.Output(output_file) 
-        a = data[:,:,0,0].sum(0)
+        nchan = data[:,:,0,0].sum(0).shape[0]
+
+        DETECTOR.MCA_chns = int(nchan)
+        DETECTOR.energy_max = data.Emax
+
+        data = SDD_convolution_with_tail(
+                data,
+                nchan,
+                3,              #scatt_order
+                DETECTOR) 
+
+        data = detector_efficiency_convolution(
+                data,
+                nchan,
+                3,
+                DETECTOR)
+
+        a = data[:,:].sum(0)
+        save = messagebox.askyesno("Save","Save output to txt?")
+        if save:
+            f = filedialog.asksaveasfile(mode="w",
+                    defaultextension=".txt",
+                    filetypes=[("Text file","*.txt")],
+                    title="Save output:")
+            if f is not None:
+                save_to_file(a,f.name)
         plt.semilogy(a)
         plt.show()
         return
+
+def save_to_file(data, path):
+    if not isinstance(data,np.ndarray):
+        print("Invalid data format")
+        return
+    f = open(path,"w+")
+    for i in range(data.shape[0]):
+        f.write(f"{data[i]}\n")
+    f.close()
 
 if __name__.endswith("__main__"):
     root = Root()
